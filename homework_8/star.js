7033456516026e75ae611b07a406d2ca073711da
Mikhail Yakovlev <kvadrofilii@gmail.com>
2021-11-20 12:45:37 +0300

    init: start homework 8 part 2

---
+++ homework_8/star.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const fibonacci =
+
+fibonacci(); // Вывод в консоль: 1
+fibonacci(); // Вывод в консоль: 1
+fibonacci(); // Вывод в консоль: 2
+fibonacci(); // Вывод в консоль: 3
+fibonacci(); // Вывод в консоль: 5
+
