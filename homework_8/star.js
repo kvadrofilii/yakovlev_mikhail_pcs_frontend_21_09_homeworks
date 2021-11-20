d759b45b01fa604218484c0e47c436fa0a25921f
Mikhail Yakovlev <kvadrofilii@gmail.com>
2021-11-20 20:23:21 +0300

    feat: finish homework 8

--- homework_8/star.js
+++ homework_8/star.js
@@ -1,10 +1,17 @@
 'use strict'
 
-const fibonacci =
+const fibonacci2 = (() => {
+  let a = 0, b = 1, c = 0;
+  return () => {
+    console.log(b);
+    c = b;
+    b = a + b;
+    a = c;
+  }
+})();
 
-fibonacci(); // Вывод в консоль: 1
-fibonacci(); // Вывод в консоль: 1
-fibonacci(); // Вывод в консоль: 2
-fibonacci(); // Вывод в консоль: 3
-fibonacci(); // Вывод в консоль: 5
-
+fibonacci2(); // Вывод в консоль: 1
+fibonacci2(); // Вывод в консоль: 1
+fibonacci2(); // Вывод в консоль: 2
+fibonacci2(); // Вывод в консоль: 3
+fibonacci2(); // Вывод в консоль: 5
