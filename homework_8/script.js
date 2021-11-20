82703362b7eb6fac82ae34498b7240e3e1911798
Mikhail Yakovlev <kvadrofilii@gmail.com>
2021-11-20 12:42:20 +0300

    feat: finish homework 8 part 1

--- homework_8/script.js
+++ homework_8/script.js
@@ -1,2 +1,20 @@
 'use strict'
 
+function makeFibonacciFunction() {
+  let a = 0, b = 1, c = 0;
+  function magic() {
+    console.log(b);
+    c = b;
+    b = a + b;
+    a = c;
+  }
+  return magic;
+}
+
+const fibonacci = makeFibonacciFunction();
+
+fibonacci(); // Вывод в консоль: 1
+fibonacci(); // Вывод в консоль: 1
+fibonacci(); // Вывод в консоль: 2
+fibonacci(); // Вывод в консоль: 3
+fibonacci(); // Вывод в консоль: 5
