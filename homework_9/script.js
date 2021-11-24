020832cb8098e1207364444c79f5f8a62193fa4c
Mikhail Yakovlev <kvadrofilii@gmail.com>
2021-11-24 14:11:38 +0300

    feat: add four point

--- homework_9/script.js
+++ homework_9/script.js
@@ -3,18 +3,17 @@
 fetch('https://reqres.in/api/users?per_page=12')
   .then(response => response.json())
   .then(body => {
-    console.log('-----------');
-    console.log('Пункт №1:')
-    console.log('-----------');
+    console.log('-----------\nПункт №1:\n-----------');
     console.log(body?.data);
-    console.log('-----------');
-    console.log('Пункт №2:')
-    console.log('-----------');
+    console.log('-----------\nПункт №2:\n-----------');
     body?.data?.forEach(item => {
       console.log(item?.last_name);
     });
-    console.log('-----------');
-    console.log('Пункт №3:')
-    console.log('-----------');
+    console.log('-----------\nПункт №3:\n-----------');
     console.log(body?.data?.filter(item => item?.last_name[0] === 'F'));
-})
+    console.log('-----------\nПункт №4:\n-----------');
+    const result = body?.data?.reduce((acc, item) => {
+      return acc + item?.first_name + ' ' + item?.last_name + ', ';
+    }, '');
+    console.log('Наша база содержит данные следующих пользователей:', result.slice(0, -2));
+  })
