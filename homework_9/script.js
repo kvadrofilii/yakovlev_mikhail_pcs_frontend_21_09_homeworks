bda0eca65ce2e1de08b7c360a0395be14c95a325
Mikhail Yakovlev <kvadrofilii@gmail.com>
2021-11-24 12:48:56 +0300

    feat: add two and three point

--- homework_9/script.js
+++ homework_9/script.js
@@ -2,8 +2,19 @@
 
 fetch('https://reqres.in/api/users?per_page=12')
   .then(response => response.json())
-  .then((body) => {
-    body?.data.forEach((item) => {
-      console.log(item);
-    })
+  .then(body => {
+    console.log('-----------');
+    console.log('Пункт №1:')
+    console.log('-----------');
+    console.log(body?.data);
+    console.log('-----------');
+    console.log('Пункт №2:')
+    console.log('-----------');
+    body?.data?.forEach(item => {
+      console.log(item?.last_name);
+    });
+    console.log('-----------');
+    console.log('Пункт №3:')
+    console.log('-----------');
+    console.log(body?.data?.filter(item => item?.last_name[0] === 'F'));
 })
