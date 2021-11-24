faf04923898302f6744e414720e0aa4596728268
Mikhail Yakovlev <kvadrofilii@gmail.com>
2021-11-24 12:05:49 +0300

    feat: add one point

--- homework_9/script.js
+++ homework_9/script.js
@@ -1,2 +1,9 @@
 'use strict'
 
+fetch('https://reqres.in/api/users?per_page=12')
+  .then(response => response.json())
+  .then((body) => {
+    body?.data.forEach((item) => {
+      console.log(item);
+    })
+})
