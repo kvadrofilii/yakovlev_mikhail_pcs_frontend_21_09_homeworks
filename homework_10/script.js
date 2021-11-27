65fc29a5cf5f0f3ea27d294311fd886d5690915b
Michael Yakovlev <kvadrofilii@gmail.com>
2021-11-27 21:58:35 +0300

    feat: finish homework 10

--- homework_10/script.js
+++ homework_10/script.js
@@ -1,19 +1,73 @@
 'use strict';
 
+const form = document.querySelector('form');
 const emailAlert = document.getElementById('email-alert');
 const passwordAlert = document.getElementById('password-alert');
 const checkboxAlert = document.getElementById('checkbox-alert');
+const checkbox = document.getElementById('checkbox');
 
+let email;
+let password;
+const objectsData = {
+	email: '',
+	password: ''
+};
+let isValid = false;
 
 const button = document.querySelector('.button');
 button.addEventListener('click', (event) => {
-    console.log('click');
-    event.preventDefault();
+  event.preventDefault();
+	emailAlert.classList.remove('not-email');
+	emailAlert.classList.remove('invalid-email');
+	passwordAlert.classList.remove('not-password');
+	passwordAlert.classList.remove('invalid-password');
+	checkboxAlert.classList.remove('not-checkbox');
+
+	if ( (email === null) || (email === '') || (email === undefined) ) {
+		emailAlert.classList.add('not-email');
+		isValid = false;
+	} else if (!validateEmail(email)) {
+		emailAlert.classList.add('invalid-email');
+		isValid = false;
+	} else {
+		objectsData.email = email;
+		isValid = true;
+	}
+
+	if ( (password === null) || (password === '') || (password === undefined) ) {
+		passwordAlert.classList.add('not-password');
+		isValid = false;
+	} else if (password.length < 8) {
+		passwordAlert.classList.add('invalid-password');
+		isValid = false;
+	} else {
+		objectsData.password = password;
+		isValid = true;
+	}
+
+	if (!checkbox.checked) {
+		checkboxAlert.classList.add('not-checkbox');
+		isValid = false;
+	}
+
+	if (isValid) {
+		console.log(objectsData);
+	}
 });
 
+const emailInput = document.querySelector('input[id=email-input]');
+emailInput.addEventListener('input', (event) => {
+  event.target.value = event.target.value.replaceAll(' ', '');
+	email = event.target.value;
+});
 
+const passwordInput = document.querySelector('input[id=password-input]');
+passwordInput.addEventListener('input', (event) => {
+  event.target.value = event.target.value.replaceAll(' ', '');
+	password = event.target.value;
+});
 
-
-function notEmail() {
-	emailAlert.style.display = 'block';
+function validateEmail(email) {
+	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
