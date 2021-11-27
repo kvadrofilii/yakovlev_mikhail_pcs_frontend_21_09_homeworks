3aba1b3ece2158445c5515474334b825e26c5433
Michael Yakovlev <kvadrofilii@gmail.com>
2021-11-27 16:17:54 +0300

    feat: add css alert states

--- homework_10/script.js
+++ homework_10/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const emailAlert = document.getElementById('email-alert');
+const passwordAlert = document.getElementById('password-alert');
+const checkboxAlert = document.getElementById('checkbox-alert');
+
+
+const button = document.querySelector('.button');
+button.addEventListener('click', (event) => {
+    console.log('click');
+    event.preventDefault();
+});
+
+
+
+
+function notEmail() {
+	emailAlert.style.display = 'block';
+}
