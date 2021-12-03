c6adf3fb1bd135dd2474248d35a5d73b612e9835
Michael Yakovlev <kvadrofilii@gmail.com>
2021-12-03 11:51:53 +0300

    refactor: validation logic

--- homework_10/script.js
+++ homework_10/script.js
@@ -10,46 +10,51 @@ let password;
 
 const button = document.querySelector('.button');
 button.addEventListener('click', (event) => {
-  event.preventDefault();
-	emailAlert.classList.remove('not-email', 'invalid-email');
-	passwordAlert.classList.remove('not-password', 'invalid-password');
-	checkboxAlert.classList.remove('not-checkbox');
+	event.preventDefault();
 
 	if (!email) {
 		emailAlert.classList.add('not-email');
 	} else if (!validateEmail(email)) {
 		emailAlert.classList.add('invalid-email');
+		emailAlert.classList.remove('not-email');
+	} else {
+		emailAlert.classList.remove('invalid-email');
 	}
 
 	if (!password) {
 		passwordAlert.classList.add('not-password');
 	} else if (password.length < 8) {
 		passwordAlert.classList.add('invalid-password');
+		passwordAlert.classList.remove('not-password');
+	} else {
+		passwordAlert.classList.remove('invalid-password');
 	}
 
 	if (!checkbox.checked) {
 		checkboxAlert.classList.add('not-checkbox');
+	} else {
+		checkboxAlert.classList.remove('not-checkbox');
 	}
 
 	if (email && validateEmail(email) && password && (password.length > 7) && checkbox.checked) {
-		console.log({email, password});
+		console.log({ email, password });
 	}
 });
 
 const emailInput = document.getElementById('email-input');
 emailInput.addEventListener('input', (event) => {
-  event.target.value = event.target.value.replaceAll(' ', '');
+	event.target.value = event.target.value.replaceAll(' ', '');
 	email = event.target.value;
 });
 
 const passwordInput = document.getElementById('password-input');
 passwordInput.addEventListener('input', (event) => {
-  event.target.value = event.target.value.replaceAll(' ', '');
+	event.target.value = event.target.value.replaceAll(' ', '');
 	password = event.target.value;
 });
 
 function validateEmail(email) {
 	const re =
-	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
 }
