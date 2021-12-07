f7510a141a837a2ad27803920e37d44dfbf55434
Michael Yakovlev <kvadrofilii@gmail.com>
2021-12-07 16:33:43 +0300

    feat: add textarea for Input component

--- diploma-project/src/components/Input/Input.jsx
+++ diploma-project/src/components/Input/Input.jsx
@@ -1,27 +1,53 @@
 function Input({ type = 'text', placeholder = '', value = '', icoBefore = '', input = '', icoAfter = '' }) {
-	if (!value) {
-		return (
-			<div className='input-container'>
-				<div className='input-wrapper'>
-					<span className={`input__ico-before ${icoBefore}`}></span>
-					<input className={`input ${input}`} type={type} placeholder={placeholder} />
-					<span className={`input__ico-after ${icoAfter}`}></span >
+
+	if (type !== 'textarea') {
+		if (!value) {
+			return (
+				<div className='input-container'>
+					<div className='input-wrapper'>
+						<span className={`input__ico-before ${icoBefore}`}></span>
+						<input className={`input ${input}`} type={type} placeholder={placeholder} />
+						<span className={`input__ico-after ${icoAfter}`}></span >
+					</div>
+					<div className={`input__text-alert ${input}`}>Поле обязательно для заполнения</div>
 				</div>
-				<div className={`input__text-alert ${input}`}>Поле обязательно для заполнения</div>
-			</div>
-		);
-	} else {
-		return (
-			<div className='input-container'>
-				<div className='input-wrapper'>
-					<span className={`input__ico-before ${icoBefore}`}></span>
-					<input className={`input ${input}`} type={type} placeholder={placeholder} defaultValue={value} />
-					<span className={`input__ico-after ${icoAfter}`}></span >
+			);
+		} else {
+			return (
+				<div className='input-container'>
+					<div className='input-wrapper'>
+						<span className={`input__ico-before ${icoBefore}`}></span>
+						<input className={`input ${input}`} type={type} placeholder={placeholder} defaultValue={value} />
+						<span className={`input__ico-after ${icoAfter}`}></span >
+					</div>
+					<div className={`input__text-alert ${input}`}>Поле обязательно для заполнения</div>
 				</div>
-				<div className={`input__text-alert ${input}`}>Поле обязательно для заполнения</div>
-			</div>
-		);
-	};
+			);
+		};
+	} if (type === 'textarea') {
+		if (!value) {
+			return (
+				<div className='input-container'>
+					<div className='input-wrapper'>
+						<textarea className={`input ${input}`} placeholder={placeholder} />
+						<span className={`input__ico-after ${icoAfter}`}></span >
+					</div>
+					<div className={`input__text-alert ${input}`}>Поле обязательно для заполнения</div>
+				</div>
+			);
+		} else {
+			return (
+				<div className='input-container'>
+					<div className='input-wrapper'>
+						<textarea className={`input ${input}`} placeholder={placeholder} defaultValue={value} />
+						<span className={`input__ico-after ${icoAfter}`}></span >
+					</div>
+					<div className={`input__text-alert ${input}`}>Поле обязательно для заполнения</div>
+				</div>
+			);
+		};
+	}
+
 }
 
 export default Input;
