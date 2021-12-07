db9753bc4231ff3ac84d8cc69779f9774c5667f6
Michael Yakovlev <kvadrofilii@gmail.com>
2021-12-07 15:44:16 +0300

    feat: add value for Input component

--- diploma-project/src/components/Input/Input.jsx
+++ diploma-project/src/components/Input/Input.jsx
@@ -1,14 +1,27 @@
-function Input({ type = 'text', placeholder = '', icoBefore = '', input = '', icoAfter = '' }) {
-	return (
-		<div className='input-container'>
-			<div className='input-wrapper'>
-				<span className={`input__ico-before ${icoBefore}`}></span>
-				<input className={`input ${input}`} type={type} placeholder={placeholder} />
-				<span className={`input__ico-after ${icoAfter}`}></span >
-			</div >
-			<div className={`input__text-alert ${input}`}>Поле обязательно для заполнения</div>
-		</div>
-	);
+function Input({ type = 'text', placeholder = '', value = '', icoBefore = '', input = '', icoAfter = '' }) {
+	if (!value) {
+		return (
+			<div className='input-container'>
+				<div className='input-wrapper'>
+					<span className={`input__ico-before ${icoBefore}`}></span>
+					<input className={`input ${input}`} type={type} placeholder={placeholder} />
+					<span className={`input__ico-after ${icoAfter}`}></span >
+				</div>
+				<div className={`input__text-alert ${input}`}>Поле обязательно для заполнения</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className='input-container'>
+				<div className='input-wrapper'>
+					<span className={`input__ico-before ${icoBefore}`}></span>
+					<input className={`input ${input}`} type={type} placeholder={placeholder} value={value} />
+					<span className={`input__ico-after ${icoAfter}`}></span >
+				</div>
+				<div className={`input__text-alert ${input}`}>Поле обязательно для заполнения</div>
+			</div>
+		);
+	};
 }
 
 export default Input;
