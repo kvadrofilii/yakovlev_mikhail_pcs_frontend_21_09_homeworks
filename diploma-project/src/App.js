fb878eec1d48989b2577adcf6303e87b67d8ebca
Michael Yakovlev <kvadrofilii@gmail.com>
2021-12-07 18:15:03 +0300

    feat: add Page404 page

--- diploma-project/src/App.js
+++ diploma-project/src/App.js
@@ -3,10 +3,10 @@ import Container from './components/Container/Container.jsx';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import Button from './components/Button/Button.jsx';
-import EditPhotoProfile from './components/EditPhotoProfile/EditPhotoProfile.jsx';
-import photo from './assets/img/users/woman/1.jpg';
-import Input from './components/Input/Input.jsx';
-import Gender from './components/Gender/Gender.jsx';
+//import EditPhotoProfile from './components/EditPhotoProfile/EditPhotoProfile.jsx';
+//import photo from './assets/img/users/woman/1.jpg';
+//import Input from './components/Input/Input.jsx';
+//import Gender from './components/Gender/Gender.jsx';
 
 
 function App() {
@@ -14,60 +14,14 @@ function App() {
     <div className='App'>
       <Header page='main' />
       <Container>
-
-
-        <Button type='link' url='/' className='button button_outline button_big' text='Назад' />
-        <div className='profile-setting-wrapper'>
-          <EditPhotoProfile url={photo} />
-
-          <form className='profile-setting__form' action="" method="post">
-            <h2 className='profile-setting__title'>Личная информация</h2>
-
-            <div className='profile-setting__inputs'>
-              <span className='profile-setting__name'>Имя</span>
-              <Input type='text' placeholder='Ваше имя' value='Jessie' icoBefore='' input='' icoAfter='' />
+        <div className='page404__wrapper'>
+          <div className='page404__content'>
+            <h1 className='page404__title'>404</h1>
+            <p className='page404__text'>Вы заблудились. Но не беспокойтесь: вот правильный путь</p>
+            <div className='page404__button-wrapper'>
+              <Button type='link' url='/' className='button button_outline page404__button' text='Вернуться назад' />
             </div>
-
-            <div className='profile-setting__inputs'>
-              <span className='profile-setting__name'>Фамилия</span>
-              <Input type='text' placeholder='Ваша фамилия' value='Rogers' icoBefore='' input='' icoAfter='' />
-            </div>
-
-            <div className='profile-setting__inputs'>
-              <span className='profile-setting__name'>Возраст</span>
-              <Input type='text' placeholder='Ваш возраст' icoBefore='ser' input='input_error' icoAfter='input_error' />
-            </div>
-
-            <div className='profile-setting__inputs'>
-              <span className='profile-setting__name'>О себе</span>
-              <Input type='textarea' placeholder='Напишите о себе' value='' input='' icoAfter='' />
-            </div>
-
-            <div className='profile-setting__inputs'>
-              <span className='profile-setting__name'>Пол</span>
-              <div className='profile-setting__gender'>
-                <Gender gender='man' isCondition='false' />
-                <Gender gender='woman' isCondition='true' />
-              </div>
-            </div>
-
-            <h2 className='profile-setting__title'>Аккаунт</h2>
-
-            <div className='profile-setting__inputs'>
-              <span className='profile-setting__name'>E-mail</span>
-              <Input type='email' placeholder='Введите e-mail' icoBefore='' input='' icoAfter='' />
-            </div>
-
-            <div className='profile-setting__inputs'>
-              <span className='profile-setting__name'>Пароль</span>
-              <Input type='password' placeholder='Введите пароль' icoBefore='' input='' icoAfter='input_eye' />
-            </div>
-
-            <div className='profile-setting__button'>
-              <Button type='button' url='/' className='button button_big button_colored' text='Сохранить' />
-            </div>
-
-          </form>
+          </div>
         </div>
 
       </Container>
--- diploma-project/src/pages/Page404/Page404.jsx
+++ diploma-project/src/pages/Page404/Page404.jsx
@@ -1,6 +1,24 @@
+import Container from './components/Container/Container.jsx';
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import Button from './components/Button/Button.jsx';
+
 function Page404() {
 	return (
 		<div>
+			<Header page='main' />
+			<Container>
+				<div className='page404__wrapper'>
+					<div className='page404__content'>
+						<h1 className='page404__title'>404</h1>
+						<p className='page404__text'>Вы заблудились. Но не беспокойтесь: вот правильный путь</p>
+						<div className='page404__button-wrapper'>
+							<Button type='link' url='/' className='button button_outline page404__button' text='Вернуться назад' />
+						</div>
+					</div>
+				</div>
+			</Container>
+			<Footer />
 		</div>
 	);
 }
